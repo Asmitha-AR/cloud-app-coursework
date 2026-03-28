5cb1030b0836bd009b3a235b132ddc9b05fb71de
KithusshandR <68683567+KithusshandR@users.noreply.github.com>
2026-03-28 18:28:18 +0530

    Update AuthDtos.cs

--- SalaryService.Api/DTOs/AuthDtos.cs
+++ SalaryService.Api/DTOs/AuthDtos.cs
@@ -2,6 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace IdentityService.Api.DTOs;
 
-public record RegisterRequest([Required][EmailAddress] string Email, [Required] string Password);
-public record LoginRequest([Required][EmailAddress] string Email, [Required] string Password);
+public record RegisterRequest(
+    [Required][EmailAddress] string Email,
+    [Required] string Password
+);
+
+public record LoginRequest(
+    [Required][EmailAddress] string Email,
+    [Required] string Password
+);
+
 public record AuthResponse(string Token, string Email);
