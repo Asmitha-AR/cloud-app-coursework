2e16b63af0cd02dd888ecab42d3e2a1a2d5ba7b9
KithusshandR <68683567+KithusshandR@users.noreply.github.com>
2026-03-28 18:32:09 +0530

    Update and rename AppDbContext.cs to SalaryDbContext.cs

---
+++ SalaryService.Api/Data/SalaryDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SalaryService.Api.Models;
+
+namespace SalaryService.Api.Data;
+
+public class SalaryDbContext : DbContext
+{
+    public SalaryDbContext(DbContextOptions<SalaryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<SalarySubmission> SalarySubmissions { get; set; }
+}
