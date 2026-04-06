daf5356895fdcb9701ae5d508cf94d69aca4d644
KithusshandR <68683567+KithusshandR@users.noreply.github.com>
2026-04-06 22:07:55 +0530

    Update Program.cs

--- StatsService.Api/Program.cs
+++ StatsService.Api/Program.cs
@@ -4,7 +4,7 @@ using StatsService.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5019");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
