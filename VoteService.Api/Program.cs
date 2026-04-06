257a860dc9018fadcd6875a656e94a02d8ad7222
KithusshandR <68683567+KithusshandR@users.noreply.github.com>
2026-04-06 22:08:14 +0530

    Update Program.cs

--- VoteService.Api/Program.cs
+++ VoteService.Api/Program.cs
@@ -7,7 +7,7 @@ using VoteService.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5002");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
